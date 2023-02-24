1a2ed1cf75360d8498d1f2398d08163596d670e0
Theo <58122097+tzebre@users.noreply.github.com>
2023-02-24 15:09:04 +0100

    push readme

--- README.md
+++ README.md
@@ -6,11 +6,10 @@ Authors :
 - OHAYON Quentin @Quentinohn
 - MATHIEU Théo @tzebre
 
-export env 
-	conda env export > environment.yml 
-Creation env 
-	conda remove -n soft_dev & conda env create -n soft_dev -f environment.yml & conda activate soft_dev
-Activation 
-	conda activate soft_dev
+build docker image :
+docker build -t python-project .
+
+run :
+docker run python-project
 
 
--- test.py
+++ test.py
@@ -1,7 +1,30 @@
 import numpy as np
 import torch
-import tkinter
-import customtkinter
+import tkinter as tk
+import customtkinter as ctk
 import math
-import PIL
-print("hello world " )
+class Application(ctk.CTk):
+    """
+    class principale de gestion de l'application
+    Creation de la fenetre
+    Creation des frame de base
+    Creation du menu
+    Appel de la classe settings
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.title("")
+        self.info()
+    def info(self):
+        w = self.winfo_screenwidth()
+        h = self.winfo_screenheight()
+        size_w = min((4 / 3) * h, w)
+        size_h = (3 / 4) * size_w
+        self.window_height = int(size_h)
+        self.window_width = int(size_w)
+        x = w // 2 - size_w // 2
+        y = h // 2 - size_h // 2
+        self.geometry("{}x{}+{}+{}".format(self.window_width, self.window_height, int(x), int(y)))
+app = Application()
+app.mainloop()
