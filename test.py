37cc63c86948b776d6670d2effd47646818b4ca1
Theo <58122097+tzebre@users.noreply.github.com>
2023-02-25 15:06:20 +0100

    debut docstring

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ img_path = "img/*.jpg"
 
 
 def random_img():
+    """
+        Retourne une image aléatoire parmi une liste d'images.
+
+        Returns:
+            str: Chemin d'accès vers l'image aléatoire sélectionnée.
+        """
     images = glob.glob(random.choice([img_path]))
     random_image = random.choice(images)
     print(random_image)
@@ -15,6 +21,15 @@ def random_img():
 
 
 def get_frame_size(frame):
+    """
+    Récupère la taille d'un cadre.
+
+    Args:
+        frame (Tkinter.Frame): Cadre dont la taille doit être récupérée.
+
+    Returns:
+        tuple: Largeur et hauteur du cadre.
+    """
     frame.update()
     w = frame.winfo_width()
     h = frame.winfo_height()
@@ -22,6 +37,16 @@ def get_frame_size(frame):
 
 
 def convert_photoimg(source, size):
+    """
+        Convertit une image source en un objet Tkinter.PhotoImage.
+
+        Args:
+            source (str): Chemin d'accès vers l'image source à convertir.
+            size (tuple): Largeur et hauteur de l'image redimensionnée.
+
+        Returns:
+            tkinter.PhotoImage: Objet PhotoImage de l'image convertie et redimensionnée.
+        """
     photo_image = ctk.CTkImage(light_image=Image.open(source), dark_image=Image.open(source),
                                size=(size[0], size[1]))
     return photo_image
@@ -34,6 +59,16 @@ class Pastchoice(ctk.CTkScrollableFrame):
         self.all_past = {}
 
     def add_img(self, source_img, size):
+        """
+            Ajoute une image à l'interface graphique sous forme de bouton avec un texte et un compteur.
+
+            Args:
+                source_img (str): Chemin d'accès vers l'image à afficher sur le bouton.
+                size (tuple): Largeur et hauteur de l'image redimensionnée.
+
+            Returns:
+                None
+        """
         self.count += 1
         old = ctk.CTkButton(self, fg_color="grey", hover_color="palegreen",
                             command=lambda count=self.count, source=source_img: self.go_past(count, source),
@@ -43,6 +78,16 @@ class Pastchoice(ctk.CTkScrollableFrame):
         self.all_past[self.count] = old
 
     def go_past(self, count, source):
+        """
+            Efface tous les boutons d'images qui se trouvent après le bouton sélectionné.
+
+            Args:
+                count (int): Numéro du bouton sélectionné.
+                source (str): Chemin d'accès vers l'image source associée au bouton sélectionné.
+
+            Returns:
+                None
+        """
         self.count = count
         print(count)
         for i in range(count + 1, len(self.all_past) + 1):
@@ -53,13 +98,6 @@ class Pastchoice(ctk.CTkScrollableFrame):
 
 
 class Application(ctk.CTk):
-    """
-    class principale de gestion de l'application
-    Creation de la fenetre
-    Creation des frame de base
-    Creation du menu
-    Appel de la classe settings
-    """
 
     def __init__(self):
         super().__init__()
@@ -69,7 +107,7 @@ class Application(ctk.CTk):
         self.window_height = None
         self.window_width = None
         self.size_photo = None
-        self.entropy_slider =None
+        self.entropy_slider = None
         self.entropy_value = None
         self.title("zero")
         self.info()
@@ -77,6 +115,15 @@ class Application(ctk.CTk):
         self.make_frame()
 
     def info(self):
+        """
+            Détermine les dimensions de la fenêtre en fonction des dimensions de l'écran et positionne la fenêtre en haut à gauche de l'écran.
+
+            Args:
+                None
+
+            Returns:
+                None
+            """
         w = self.winfo_screenwidth()
         h = self.winfo_screenheight()
         size_w = min((4 / 3) * h, w)
@@ -90,6 +137,18 @@ class Application(ctk.CTk):
         self.geometry("{}x{}+{}+{}".format(w, h, int(x), int(y)))
 
     def make_frame(self):
+        """
+            Configure et crée les frames nécessaires pour l'interface graphique de l'application.
+
+            Cette méthode configure les colonnes et les rangées de la frame principale, puis crée deux sous-frames :
+            une frame "left" et une frame "right".
+            La frame "left" contient un widget "Pastchoice" qui permet à l'utilisateur de naviguer parmi les images déjà traitées.
+            La frame "right" contient une sous-frame "top" et une sous-frame "btm". La sous-frame "top" est utilisée pour afficher les images
+            traitées et la sous-frame "btm" contient un slider, un label et un bouton de validation.
+
+            Returns:
+                None
+            """
         self.grid_columnconfigure(0, weight=1, uniform="group1")
         self.grid_columnconfigure(1, weight=5, uniform="group1")
         self.grid_rowconfigure(0, weight=1)
@@ -116,7 +175,7 @@ class Application(ctk.CTk):
         btm.columnconfigure(2, weight=1, uniform="group2")
         self.entropy_slider = ctk.CTkSlider(master=btm, from_=0, to=100, command=self.change_value)
         self.entropy_slider.grid(row=0, column=0, sticky="nsew")
-        self.entropy_value = ctk.CTkLabel(master = btm, text=f"entropy : {self.entropy_slider.get():.02f}")
+        self.entropy_value = ctk.CTkLabel(master=btm, text=f"entropy : {self.entropy_slider.get():.02f}")
         self.entropy_value.grid(row=0, column=1, sticky="nsew")
         end = ctk.CTkButton(btm, text="Validation finale")
         end.grid(row=0, column=2)
@@ -145,9 +204,24 @@ class Application(ctk.CTk):
                 h = h // 2.5
                 self.size_photo = (w, h)
         self.new_image()
+
     def change_value(self, val):
-        self.entropy_value.configure(text = f"entropy : {val:.02f}")
+        """Met à jour le texte du label de l'entropie en fonction de la valeur donnée.
+
+            Args:
+                val (float): La nouvelle valeur de l'entropie.
+        """
+        self.entropy_value.configure(text=f"entropy : {val:.02f}")
+
     def button_click(self, coords):
+        """Change l'état d'un bouton de photo lorsqu'il est cliqué.
+
+                Args:
+                    coords (tuple): Les coordonnées du bouton de photo cliqué.
+
+                Returns:
+                    None
+        """
         r = coords[0]
         c = coords[1]
         focus_btn = self.photo_frame[r][c]
@@ -162,12 +236,29 @@ class Application(ctk.CTk):
             self.new_image()
 
     def unselect_all(self):
+        """
+            Désélectionne tous les boutons de l'image.
+
+            Parcourt la grille de boutons de l'image et désélectionne chacun d'eux, en
+            changeant leur couleur de police en gris et en marquant le bouton comme non
+            cliqué.
+
+        """
         for r in range(2):
             for c in range(3):
                 self.photo_frame[r][c]['btn'].configure(fg_color="grey")
                 self.photo_frame[r][c]['clicked'] = False
 
     def new_image(self):
+        """
+        Génère de nouvelles images pour chaque bouton de la grille photo.
+
+            Args:
+                Aucun.
+
+            Returns:
+                Aucun.
+        """
         for r in range(2):
             for c in range(3):
                 source = random_img()
