5c1c7e5b3d36de82077c4b19048b3d615155d439
Theo <58122097+tzebre@users.noreply.github.com>
2023-03-31 16:31:42 +0200

    ready to deploy

--- execution.sh
+++ execution.sh
@@ -1,18 +1,20 @@
-#!usr/bin
-SCRIPT=$(readlink -f "$_")
-SCRIPT_DIR=$(dirname "$SCRIPT")
+#SCRIPT=$(readlink -f "$_")
+#SCRIPT_DIR=$(dirname "$SCRIPT")
+#replace by path
+#alias app_auth="source /Users/quentinduvert/Desktop/projet_dev_log/info_7/execution.sh"
+SCRIPT_DIR="/Users/theomathieu/Documents/cours/4A/S2/dev_log/code/info_7"
+
 conda >/dev/null
 if [ $? -ne 0 ]; then
-  Distrib=$uname
-  if [ $expr $Distrib = 'Darwin' ]; then
-    curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh" -o SCRIPT_DIR/Miniconda3-latest-MacOSX-x86_64.sh
-    bash SCRIPT_DIR/Miniconda3-latest-MacOSX-x86_64.sh
+  if [[ $(uname) == "Darwin" ]]; then
+    curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh" -o $SCRIPT_DIR/Miniconda3-latest-MacOSX-x86_64.sh
+    bash $SCRIPT_DIR/Miniconda3-latest-MacOSX-x86_64.sh
     chemin=$(whereis conda | awk '{print $2}')
     $chemin init
     conda >/dev/null
   else
-    curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" -o SCRIPT_DIR/Miniconda3-latest-Linux-x86_64.sh
-    bash SCRIPT_DIR/Miniconda3-latest-Linux-x86_64.sh
+    curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" -o $SCRIPT_DIR/Miniconda3-latest-Linux-x86_64.sh
+    bash $SCRIPT_DIR/Miniconda3-latest-Linux-x86_64.sh
     chemin=$(whereis conda | awk '{print $2}')
     $chemin init
     conda >/dev/null
@@ -26,16 +28,17 @@ if [ $? -ne 0 ]; then
 else
   echo "Conda est déjà installé"
 fi
-if [ $expr $Distrib = 'Darwin' ]; then
-  env=env.yml
+if [[ $(uname) == "Darwin" ]]; then
+  env="env.yml"
 else
-  env=env_linux.yml
+  env="env_linux.yml"
 fi
 conda info --envs | grep "Enviro_group_zero" >/dev/null
 if [ $? -eq 0 ]; then
   echo "Environnement déjà présent"
   conda activate Enviro_group_zero
   conda list >$SCRIPT_DIR/.comp_tmp.txt
+  echo $SCRIPT_DIR
   cmp $SCRIPT_DIR/.comp_tmp.txt $SCRIPT_DIR/.env.txt
   if [ $? -eq 0 ]; then
     echo "L'environnement est correctement chargé"
@@ -47,11 +50,12 @@ if [ $? -eq 0 ]; then
 fi
 conda info --envs | grep "Enviro_group_zero" >/dev/null
 if [ $? -ne 0 ]; then
-  conda env create -f $env
+  conda env create -f $SCRIPT_DIR/$env
   if [ $? -eq 0 ]; then
     echo "L'environnement a été créé succès"
     conda activate Enviro_group_zero
     conda list >$SCRIPT_DIR/.env.txt
+    conda list >$SCRIPT_DIR/.comp_tmp.txt
   else
     echo "L'environnement n'a pas été créé"
     exit 1
