9abe4c83fc83a59f0f9185d6bf4ad7fa01bd8fa1
QuentinDvt <qduvert@gmail.com>
2023-03-31 11:00:40 +0200

    resol pb install

--- execution.sh
+++ execution.sh
@@ -3,13 +3,17 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 conda > /dev/null
 if [ $? -ne 0 ]; then
     Distrib=$uname
-    if [ $expr $Distrib='Darwin' ]; then
+    if [ $expr $Distrib = 'Darwin' ]; then
         curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh" -o SCRIPT_DIR/Miniconda3-latest-MacOSX-x86_64.sh
         bash SCRIPT_DIR/Miniconda3-latest-MacOSX-x86_64.sh
+        chemin=$(whereis conda | awk '{print $2}')
+        $chemin init
         conda  > /dev/null
     else
         curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" -o SCRIPT_DIR/Miniconda3-latest-Linux-x86_64.sh
         bash SCRIPT_DIR/Miniconda3-latest-Linux-x86_64.sh
+        chemin=$(whereis conda | awk '{print $2}')
+        $chemin init
         conda > /dev/null
     fi
     if [ $? -eq 0 ]; then
@@ -25,14 +29,22 @@ conda info --envs | grep "Enviro_group_zero" > /dev/null
 if [ $? -eq 0 ]; then
     echo "Environnement déjà présent"
     conda activate Enviro_group_zero
-    conda list > SCRIPT_DIR/.comp_tmp.txt
-    cmp SCRIPT_DIR/.comp_tmp.txt SCRIPT_DIR/.env.txt
+    conda list > $SCRIPT_DIR/.comp_tmp.txt
+    pip list > $SCRIPT_DIR/.pip_tmp.txt
+    cmp $SCRIPT_DIR/.comp_tmp.txt $SCRIPT_DIR/.env.txt
     if [ $? -eq 0 ]; then
         echo "L'environnement est correctement chargé"
     else
         echo "L'environnement n'est pas correctement chargé"
         conda activate base
         conda remove -n Enviro_group_zero --all
+    fi
+    cmp $SCRIPT_DIR/.comp_tmp.txt S$CRIPT_DIR/.pip_env.txt
+    if [ $? -eq 0 ]; then
+        echo "L'environnement est correctement chargé"
+    else
+        echo "L'environnement n'est pas correctement chargé"
+        pip install -r $SCRIPT_DIR/requirement.txt
     fi
 fi
 conda info --envs | grep "Enviro_group_zero" > /dev/null
@@ -41,15 +53,16 @@ if [ $? -ne 0 ]; then
     if [ $? -eq 0 ]; then
         echo "L'environnement a été créé succès"
         conda activate Enviro_group_zero
-        conda list > SCRIPT_DIR/.env.txt
+        conda list > $SCRIPT_DIR/.env.txt
+        pip list > $SCRIPT_DIR/.pip_env.txt
     else
         echo "L'environnement n'a pas été créé"
         exit 1
     fi
 fi
 
-rm SCRIPT_DIR/.comp_tmp.txt
-cd SCRIPT_DIR/Module
+rm $SCRIPT_DIR/.comp_tmp.txt
+cd $SCRIPT_DIR/Module
 python3 main.py
 cd -
 
